9df29dd61a021f0d191897cfefa1a391675aed66
ACKALUUBOT <ackaluubotz@gmail.com>
2026-06-15 13:42:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv('8839262149:AAHq4oOk46Nyj7Fy4RZK5vMfYfgfn8ETEN0')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 MONGO_URI = os.getenv('MONGO_URI')
 ADMIN_ID = int(os.getenv('ADMIN_ID', 0))
 UPI_ID = os.getenv('UPI_ID')
